f33f9d95f160b3711d37252855d49436bc99f803
pragnakalpdev85 <pragnakalp.dev85@gmail.com>
2026-07-28 13:27:59 +0530

    test: testing cicd pipeline

--- commands.sh
+++ commands.sh
@@ -2,7 +2,6 @@
 cd Book_Library_System/library
 python manage.py migrate --noinput 
 
-# start the gunicorn worker processws at the defined port
 gunicorn --bind 0.0.0.0:8000 --workers 3 library.wsgi:application --access-logfile - --error-logfile - &
 
 wait

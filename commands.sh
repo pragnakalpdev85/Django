309c387406071a369201052bc32027b47baa13d2
pragnakalpdev85 <pragnakalp.dev85@gmail.com>
2026-07-29 15:59:26 +0530

    test: cicd pipeline

--- commands.sh
+++ commands.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+set -e
+
 # Apply migrations
-cd Book_Library_System/library
+cd /app/Book_Library_System/library
 python manage.py migrate --noinput 
 
 gunicorn --bind 0.0.0.0:8000 --workers 3 library.wsgi:application --access-logfile - --error-logfile - &
